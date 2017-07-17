0b4f0550f77d303bd2aadc67ba3f4ed21ca846c4
Alex <alex@ruzzarin.net>
2017-07-17 16:33:39 -0400

    fix: remove resource file and fix JS module (#2)

--- www/huella.js
+++ www/huella.js
@@ -1,19 +1,15 @@
-var argscheck = require('cordova/argscheck'),
-    exec = require('cordova/exec');
-
+var exec = require('cordova/exec');
 
 function Huella() {
 }
 
 Huella.prototype.isAvailable = function() {
-    argscheck.checkArgs('fF', 'Huella.isAvailable', arguments);
     return new Promise((resolve, reject) => {
       exec(resolve, reject, "Huella", "isAvailable", []);
     });
 };
 
-Huella.prototype.showUnlock = function(successCallback, errorCallback) {
-    argscheck.checkArgs('fF', 'Huella.showUnlock', arguments);
+Huella.prototype.showUnlock = function() {
     return new Promise((resolve, reject) => {
       exec(resolve, reject, "Huella", "showUnlock", []);
     });
